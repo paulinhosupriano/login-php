8ff3f6306e4d15df8cc5a474cfb52fc24220b833
PaulinhoSupriano Developer <30899341+paulinhosupriano@users.noreply.github.com>
2022-05-30 10:39:25 -0300

    Update auth.php
    melhorando o comentário da aplicação.

--- php/auth.php
+++ php/auth.php
@@ -5,28 +5,28 @@ $redirectAuth = '../index.php';
 
 $data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
-// verifica se existe dados.
+// verifica existencia de dados.
 if (!isset($data)) {
     setMessage("Ops, favor informe os dados.", 'info');
     redirect($redirectAuth);
     exit;
 }
 
-// verifica se existe dados.
+// checa se está vazios os campos informados.
 if(in_array('',$data)){
     setMessage("Ops, preencha os dados.",'info');
     redirect($redirectAuth);
     exit;
 }
 
-// Verifica se existe e-mail.
+// Valida e-mail chegando formado válido email@email.com .
 if(!isMail($data['email'])){
     setMessage("Formato inválido",'info');
     redirect($redirectAuth);
     exit;
 }
 
-// validar e-mail.
+// efetua checagem de autenticação.
 if(!auth($data['email'],$data['senha'])){
     setMessage("Dados incorretos",'danger');
     redirect($redirectAuth);
