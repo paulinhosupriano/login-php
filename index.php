393c9dd433c8e455cf785fb5db4ae0d988b49e10
PaulinhoSupriano <paulinhosupriano@gmail.com>
2022-05-30 01:18:36 -0300

    melhorias

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LOGIN</title>
-
     <link rel="stylesheet" href="assets/bootstrap-5.2.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/login.css">
 </head>
